251aa73810dcd29865267a9771cfb954c905ec3f
tim-schaeren <tim.schaeren@gmail.com>
2023-10-30 14:54:21 +0100

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Roboto_Mono, Syne } from 'next/font/google';
+
+export const roboto = Roboto_Mono({ subsets: ['latin'] });
+export const syne = Syne({
+    subsets: ['latin'],
+    weight: '400',
+});
